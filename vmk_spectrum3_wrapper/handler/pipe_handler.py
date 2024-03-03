0f02defbaa425e633b2f983acd09627d61558899
Exinker <exinker@gmail.com>
2024-03-03 15:33:33 +0700

    feat: set pipe handler by default

--- vmk_spectrum3_wrapper/handler/pipe_handler.py
+++ vmk_spectrum3_wrapper/handler/pipe_handler.py
@@ -4,18 +4,27 @@ import time
 import numpy as np
 
 from vmk_spectrum3_wrapper.handler.base_handler import BaseHandler
-from vmk_spectrum3_wrapper.handler import BufferHandler, Handler
+from vmk_spectrum3_wrapper.handler import BufferHandler, Handler, ScaleHandler
 from vmk_spectrum3_wrapper.typing import Array, Digit, Second, T
 
 
 class PipeHandler(BaseHandler):
 
-    def __init__(self, __handlers: Sequence[Handler | BufferHandler]):
-        self._handlers = __handlers
+    def __init__(self, __handlers: Sequence[Handler | BufferHandler] | None = None):
+        if __handlers:
+            assert isinstance(__handlers[0], ScaleHandler), '`ScaleHandler` have to be the first in sequence!'
+
+        self._handlers = __handlers or [
+            ScaleHandler(),
+        ]
 
     @property
     def handlers(self) -> Sequence[Handler]:
         return self._handlers
+
+    @property
+    def units(self) -> Units:
+        return self.handlers[0].units
 
     # --------        private        --------
     def __call__(self, data: Array[Digit], *args, **kwargs) -> Array[T]:
