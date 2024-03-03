c994ceb1f93509b33d066e5999fe5be72b0b2f1f
Exinker <exinker@gmail.com>
2024-03-03 15:10:01 +0700

    feat: set True all handlers by default

--- vmk_spectrum3_wrapper/handler/base_handler.py
+++ vmk_spectrum3_wrapper/handler/base_handler.py
@@ -6,6 +6,9 @@ from vmk_spectrum3_wrapper.typing import Array, T
 class BaseHandler(ABC):
 
     # --------        private        --------
+    def __bool__(self) -> bool:
+        return True
+
     @abstractmethod
     def __call__(self, data: Array[T], *args, **kwargs) -> Array[T]:
         raise NotImplementedError
