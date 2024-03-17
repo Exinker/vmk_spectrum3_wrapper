67aebc8a08d0c7f92bbabf9f9e57ca4c83d3af76
Exinker <exinker@gmail.com>
2024-03-17 15:03:47 +0700

    feat: add swap handler

--- vmk_spectrum3_wrapper/handler/handler.py
+++ vmk_spectrum3_wrapper/handler/handler.py
@@ -7,8 +7,37 @@ class Handler(BaseHandler):
     """Not reduce dimension handlers."""
 
 
+class SwapHandler(Handler):
+    """Handler to swap a datum if needed."""
+
+    def __init__(self, flag: bool = False):
+        self._flag = flag
+
+    @property
+    def flag(self) -> bool:
+        return self._flag
+
+    # --------        private        --------
+    def __call__(self, datum: Datum, *args, **kwargs) -> Datum:
+
+        if not self.flag:
+            return datum
+
+        return Datum(
+            intensity=datum.intensity.flip(),
+            units=datum.units,
+            clipped=datum.clipped.flip(),
+            meta=Meta(
+                capacity=datum.meta.capacity,
+                exposure=datum.meta.exposure,
+                started_at=datum.meta.started_at,
+                finished_at=datum.meta.finished_at,
+            ),
+        )
+
+
 class ScaleHandler(Handler):
-    """Handler to scale a data from `Units.digit` to `units`"""
+    """Handler to scale a datum from `Units.digit` to `units`"""
 
     def __init__(self, units: Units | None = None):
         self._units = units or Units.percent
@@ -23,31 +52,47 @@ class ScaleHandler(Handler):
         return self._scale
 
     # --------        private        --------
-    def __call__(self, data: Datum, *args, **kwargs) -> Datum:
-        assert data.units == Units.digit, 'ScaleHandler: {data.units} is not valid! Only `digit` is supported!'
+    def __call__(self, datum: Datum, *args, **kwargs) -> Datum:
+        assert datum.units == Units.digit, 'ScaleHandler: {datum.units} is not valid! Only `digit` is supported!'
 
         return Datum(
-            intensity=self.scale*data.intensity,
+            intensity=self.scale*datum.intensity,
             units=self.units,
-            clipped=data.clipped,
+            clipped=datum.clipped,
             meta=Meta(
-                capacity=data.meta.capacity,
-                exposure=data.meta.exposure,
-                started_at=data.meta.started_at,
-                finished_at=data.meta.finished_at,
+                capacity=datum.meta.capacity,
+                exposure=datum.meta.exposure,
+                started_at=datum.meta.started_at,
+                finished_at=datum.meta.finished_at,
             ),
         )
 
 
-class DarkCalibration(BaseHandler):
+class OffsetHandler(BaseHandler):
 
-    def __init__(self, dark: Data):
-        self._dark = dark
+    def __init__(self, offset: Data):
+        self._offset = offset
 
     @property
-    def dark(self) -> Data:
-        return self._dark
+    def offset(self) -> Data:
+        return self._offset
+
+    @property
+    def units(self) -> Units:
+        return self.offset.units
 
     # --------        private        --------
     def __call__(self, datum: Datum, *args, **kwargs) -> Datum:
-        return datum - self.dark
+        assert datum.units == self.units
+
+        return Datum(
+            intensity=datum.intensity - self.offset.intensity,
+            units=datum.units,
+            clipped=datum.clipped | self.offset.clipped,
+            meta=Meta(
+                capacity=datum.meta.capacity,
+                exposure=datum.meta.exposure,
+                started_at=datum.meta.started_at,
+                finished_at=datum.meta.finished_at,
+            ),
+        )
