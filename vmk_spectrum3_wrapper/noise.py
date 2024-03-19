9ab3fca853cc8c6929ea35269c2e429be5575111
Exinker <exinker@gmail.com>
2024-03-19 09:03:59 +0700

    feat: add `digit` units

--- vmk_spectrum3_wrapper/noise.py
+++ vmk_spectrum3_wrapper/noise.py
@@ -3,32 +3,41 @@ from typing import overload
 
 import numpy as np
 
+from vmk_spectrum3_wrapper.adc import ADC
 from vmk_spectrum3_wrapper.detector import Detector
-from vmk_spectrum3_wrapper.typing import Array, Electron, Percent, U
+from vmk_spectrum3_wrapper.typing import Array, U
+from vmk_spectrum3_wrapper.units import Units
 
 
 @dataclass(frozen=True)
 class Noise():
     """Detector's noise dependence."""
     detector: Detector
-    n_frames: int
-    units: U = field(default=Percent)
+    adc: ADC = field(default=ADC._16bit)
+    units: U = field(default=Units.percent)
+    n_frames: int = field(default=1)
 
     @overload
     def __call__(self, value: U) -> U: ...
     @overload
     def __call__(self, value: Array[U]) -> Array[U]: ...
-    @overload
     def __call__(self, value):
+        adc = self.adc
         detector = self.detector
         n_frames = self.n_frames
 
-        if self.units == Electron:
+        if self.units == Units.digit:
+            read_noise = detector.config.read_noise
+            kc = detector.config.capacity / adc.value_max
+
+            return (1/kc) * np.sqrt(read_noise**2 + value*kc) / np.sqrt(n_frames)
+
+        if self.units == Units.electron:
             read_noise = detector.config.read_noise
 
             return np.sqrt(read_noise**2 + value) / np.sqrt(n_frames)
 
-        if self.units == Percent:
+        if self.units == Units.percent:
             read_noise = detector.config.read_noise
             kc = detector.config.capacity / 100
 
