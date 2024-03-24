00fefb127568e759ae3988e6ea91ec5aa7383fa5
Exinker <exinker@gmail.com>
2024-03-24 16:40:19 +0700

    refac:

--- vmk_spectrum3_wrapper/filter/pipe_filter.py
+++ vmk_spectrum3_wrapper/filter/pipe_filter.py
@@ -1,25 +1,23 @@
 from collections.abc import Sequence
+from typing import NewType
 
-from vmk_spectrum3_wrapper.data import Data, Datum
+from vmk_spectrum3_wrapper.data import Datum
 from vmk_spectrum3_wrapper.filter.base_filter import BaseFilter
-from vmk_spectrum3_wrapper.filter.buffer_filter import BufferFilter, IntegrationFilter
-from vmk_spectrum3_wrapper.filter.flow_filter import ClipFilter, DeviationFilter, FlowFilter, OffsetFilter, ScaleFilter, ShuffleFilter
-from vmk_spectrum3_wrapper.shuffle import Shuffle
 from vmk_spectrum3_wrapper.units import Units
 
 
-class PipeFilter(BaseFilter):
+F = NewType('F', BaseFilter)
 
-    def __init__(self, filters: Sequence[FlowFilter | BufferFilter]):
+
+class PipeFilter(BaseFilter):
+    """Конвейер фильтров."""
+
+    def __init__(self, filters: Sequence[F]):
         self._filters = [item for item in filters if item is not None]
 
     @property
-    def filters(self) -> Sequence[FlowFilter]:
+    def filters(self) -> Sequence[F]:
         return self._filters
-
-    @property
-    def units(self) -> Units:
-        return self.filters[0].units
 
     # --------        private        --------
     def __call__(self, datum: Datum, *args, **kwargs) -> Datum:
@@ -33,43 +31,5 @@ class PipeFilter(BaseFilter):
 
         return datum
 
-    def __getitem__(self, index: int) -> FlowFilter:
+    def __getitem__(self, index: int) -> F:
         return self.filters[index]
-
-
-# --------        presets        --------
-class CoreFilterPreset(PipeFilter):
-
-    def __init__(
-        self,
-        shuffle: Shuffle | None = None,
-        units: Units = None,
-        bias: Data | None = None,
-        dark: Data | None = None,
-    ):
-        units = units or Units.percent
-
-        super().__init__(filters=[
-            ShuffleFilter(shuffle) if isinstance(shuffle, Shuffle) else None,
-            ClipFilter(),
-            ScaleFilter(units=units),
-            OffsetFilter(bias) if isinstance(bias, Data) else None,
-            DeviationFilter(units=units) if isinstance(bias, Data) else None,
-            OffsetFilter(dark) if isinstance(dark, Data) else None,
-        ])
-
-
-class IntegrationFilterPreset(PipeFilter):
-
-    def __init__(
-        self,
-        shuffle: Shuffle | None = None,
-        units: Units | None = None,
-        bias: Data | None = None,
-        dark: Data | None = None,
-        is_averaging: bool = True,
-    ):
-        super().__init__(filters=[
-            CoreFilterPreset(shuffle=shuffle, units=units, bias=bias, dark=dark),
-            IntegrationFilter(is_averaging=is_averaging),
-        ])
