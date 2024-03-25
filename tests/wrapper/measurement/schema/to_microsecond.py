9b1c1a7df266785fd680ddf302525a699ce58276
Pavel Vashchenko <exinker@gmail.com>
2024-03-25 22:29:25 +0700

    tests: to_microsecond

---
+++ tests/wrapper/measurement/schema/to_microsecond.py
@@ -0,0 +1,30 @@
+import pytest
+
+from vmk_spectrum3_wrapper.exception import SetupDeviceError
+from vmk_spectrum3_wrapper.measurement.schema import to_microsecond
+from vmk_spectrum3_wrapper.typing import MicroSecond, MilliSecond
+
+
+@pytest.mark.parametrize(
+    ['exposure', 'expected'],
+    [
+        (0, 0),
+        (0.4, 400),
+        (1., 1_000),
+        (1, 1_000),
+        (10, 10_000),
+    ],
+)
+def test_to_microsecond(exposure: MilliSecond, expected: MicroSecond):
+    assert to_microsecond(exposure) == expected
+
+
+@pytest.mark.parametrize(
+    ['exposure', 'expected'],
+    [
+        (0.42, SetupDeviceError),
+    ],
+)
+def test_to_microsecond_fail(exposure: MilliSecond, expected: SetupDeviceError):
+    with pytest.raises(expected):
+        to_microsecond(exposure)
