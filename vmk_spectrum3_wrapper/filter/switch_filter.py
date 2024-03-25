3a2780a3eb4f7fc819d203d014da5588863e15de
Exinker <exinker@gmail.com>
2024-03-25 14:28:49 +0700

    feat: add split and merge functions

--- vmk_spectrum3_wrapper/filter/switch_filter.py
+++ vmk_spectrum3_wrapper/filter/switch_filter.py
@@ -1,7 +1,8 @@
 from collections.abc import Sequence
 
+import numpy as np
+
 from vmk_spectrum3_wrapper.data import Datum
-from vmk_spectrum3_wrapper.data.utils import crop, join
 
 from .base_filter import BaseFilter
 from .pipe_filter import PipeFilter
@@ -28,39 +29,46 @@ class SwitchFilter(BaseFilter):
             except Exception as error:
                 print(error)
 
-        spam = join_shots(shots)
-
-        return spam
+        return merge_shots(shots)
 
     def __getitem__(self, index: int) -> PipeFilter:
         return self.filters[index]
 
 
 # --------        utils        --------
-def split_shots(datum: Datum, capacity: tuple[int, int], n0: int = 0) -> list[Datum]:
+def split_shots(datum: Datum, capacity: tuple[int, int], t0: int = 0) -> list[Datum]:
+    """Разделить `datum` по `capacity` на несколько (два)."""
 
-    shots = []
-    for dn in capacity:
-        index = slice(n0, n0+dn)
-        shots.append(
-            Datum(
-                intensity=crop(datum.intensity, index),
-                units=datum.units,
-                clipped=crop(datum.clipped, index),
-                deviation=crop(datum.deviation, index),
-            ),
-        )
+    def inner(capacity: tuple[int, int]) -> slice:
+        t0 = 0
 
-        n0 += dn
+        for dt in capacity:
+            yield slice(t0, t0+dt)
 
-    #
-    return shots
+            t0 += dt
+
+    return [
+        datum[index, :]
+        for index in inner(capacity)
+    ]
 
 
-def join_shots(shots: Sequence[Datum]) -> Datum:
+def merge_shots(shots: Sequence[Datum]) -> Datum:
+    """Слить несколько `shots` в один `datum`."""
+    def inner(values):
+        try:
+            return np.concatenate(values, axis=0)
+        except Exception as error:
+            print(error)
+
+        return None
+
+    for i, shot in enumerate(shots):
+        print(f'short({i}): {shot.n_times}x{shot.n_numbers}', shot.intensity, shot.clipped)
+
     return Datum(
-        intensity=join([shot.intensity for shot in shots]),
+        intensity=inner([shot.intensity for shot in shots]),
         units=shots[0].units,
-        clipped=join([shot.clipped for shot in shots]),
-        deviation=join([shot.deviation for shot in shots]),
+        clipped=inner([shot.clipped for shot in shots]),
+        deviation=inner([shot.deviation for shot in shots]),
     )
