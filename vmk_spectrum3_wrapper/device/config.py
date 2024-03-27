19d32239a29fd9456110ce3f283992c9fe11a500
Pavel Vashchenko <exinker@gmail.com>
2024-03-27 23:19:48 +0700

    refac: remove unused constants

--- vmk_spectrum3_wrapper/device/config.py
+++ vmk_spectrum3_wrapper/device/config.py
@@ -2,14 +2,10 @@ from collections.abc import Sequence
 from dataclasses import dataclass, field
 from typing import TypeAlias
 
-from vmk_spectrum3_wrapper.adc import ADC
-from vmk_spectrum3_wrapper.detector import Detector
 from vmk_spectrum3_wrapper.typing import IP, MilliSecond
 
 
 CHANGE_EXPOSURE_DELAY = 1000
-_ADC = ADC._16bit
-_DETECTOR = Detector.BLPP2000
 
 
 @dataclass
