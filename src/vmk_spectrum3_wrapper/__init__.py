3c6716ec079eb59327a70f381e4d75710bcffd44
Exinker <exinker@gmail.com>
2024-12-23 10:31:28 +0700

    refac: add uv package manager

--- src/vmk_spectrum3_wrapper/__init__.py
+++ src/vmk_spectrum3_wrapper/__init__.py
@@ -1,11 +1,17 @@
+import dotenv 
+import pkg_resources
+
 from vmk_spectrum3_wrapper.loggers import setdefault_logger
 
+
+dotenv.load_dotenv()
 setdefault_logger()
 
 
-NAME = 'vmk_spectrum3_wrapper'
+distribution = pkg_resources.get_distribution('vmk-spectrum3-wrapper')
+NAME = distribution.project_name
 DESCRIPTION = 'It is a wrapper for `vmk_spectrum3` library.'
-VERSION = '0.2.5'
+VERSION = distribution.version
 
 AUTHOR_NAME = 'Pavel Vaschenko'
 AUTHOR_EMAIL = 'vaschenko@vmk.ru'
